ea1a9502d81a4591e5d5a85f5e675ab5153f005a
ysjprojects <85871692+ysjprojects@users.noreply.github.com>
2022-12-14 12:23:37 +0800

    feat: included Typescript config option ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     dirs: ['src'],
   },
 
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+
   reactStrictMode: true,
   swcMinify: true,
 
